2f758536d23236beeea436656e37ec94d62150a4
Zappy <adezapelectronics@gmail.com>
2026-04-17 18:51:39 +0100

    Clean up unused imports and format code

--- lib/main.dart
+++ lib/main.dart
@@ -1365,4 +1365,4 @@ class _ClaimScreenState extends State<ClaimScreen> {
   }
 }
 
-// [AUTO-SYNC] Minor optimizations checked at: 2026-04-13 04:35:53
+// [AUTO-SYNC] Minor optimizations checked at: 2026-04-17 18:51:39
