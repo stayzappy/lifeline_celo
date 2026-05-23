40bc92d3b24311ea6d17a881dacf28b8661b03eb
Zappy <adezapelectronics@gmail.com>
2026-05-23 15:23:35 +0100

    style: refine visual consistency (15:22)

--- lib/theme/runtime_theme.g.dart
+++ lib/theme/runtime_theme.g.dart
@@ -1,7 +1,7 @@
 // GENERATED FILE
-// Generated at: 2026-05-23 14:50:19
+// Generated at: 2026-05-23 15:22:38
 
 class RuntimeTheme {
   static const String accentColor = '#FFE500';
-  static const String generatedAt = '2026-05-23 14:50:19';
+  static const String generatedAt = '2026-05-23 15:22:38';
 }
