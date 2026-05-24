46fd2c03276a57804117e9736c93b54a6fcc1188
Zappy <adezapelectronics@gmail.com>
2026-05-24 09:33:29 +0100

    style: update branding palette (09:32)

--- lib/theme/runtime_theme.g.dart
+++ lib/theme/runtime_theme.g.dart
@@ -1,7 +1,7 @@
 // GENERATED FILE
-// Generated at: 2026-05-23 22:27:32
+// Generated at: 2026-05-24 09:32:26
 
 class RuntimeTheme {
   static const String accentColor = '#BF5FFF';
-  static const String generatedAt = '2026-05-23 22:27:32';
+  static const String generatedAt = '2026-05-24 09:32:26';
 }
