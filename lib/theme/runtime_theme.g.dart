54f6c5e85dd6297cf37ccf5b2188501c3dce6632
Zappy <adezapelectronics@gmail.com>
2026-05-24 20:22:37 +0100

    style: improve onboarding transitions (20:21)

--- lib/theme/runtime_theme.g.dart
+++ lib/theme/runtime_theme.g.dart
@@ -1,7 +1,7 @@
 // GENERATED FILE
-// Generated at: 2026-05-24 20:20:29
+// Generated at: 2026-05-24 20:21:45
 
 class RuntimeTheme {
-  static const String accentColor = '#FF9500';
-  static const String generatedAt = '2026-05-24 20:20:29';
+  static const String accentColor = '#C8FF00';
+  static const String generatedAt = '2026-05-24 20:21:45';
 }
