7bf6bba5d21e95137c710edecf5620344ff5022e
Zappy <adezapelectronics@gmail.com>
2026-06-21 18:48:21 +0100

    build: refresh flutter web assets (18:48)

--- lib/theme/runtime_theme.g.dart
+++ lib/theme/runtime_theme.g.dart
@@ -1,7 +1,7 @@
 // GENERATED FILE
-// Generated at: 2026-06-21 18:47:17
+// Generated at: 2026-06-21 18:48:02
 
 class RuntimeTheme {
   static const String accentColor = '#FF0080';
-  static const String generatedAt = '2026-06-21 18:47:17';
+  static const String generatedAt = '2026-06-21 18:48:02';
 }
